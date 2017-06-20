0e22b5bb2827580cf1ab31b88e4467dfd003bd7a
forest-shadow <mamasay@yandex.ru>
2017-06-20 13:40:04 +0300

    add ask-question test app with angular filters implementation

---
+++ ask-question-app/app/app.module.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function(){
+    let app = angular.module( 'askQuestion', ['ngMaterial'] );
+
+    app.controller( 'askQuestionController', function( $scope ) {
+        $scope.question = {
+            text:   'Which js-framework is better to use?',
+            author: 'John Johns',
+            date:   '20/10/2013',
+            answers: [
+                {
+                    text:   'AngularJS!',
+                    author: 'Peter Smith',
+                    date:   '20/10/2013',
+                    rate:   2
+                },
+                {
+                    text:   'AngularJS better than all',
+                    author: 'David Flanigan',
+                    date:   '15/10/2013',
+                    rate:   0
+                },
+                {
+                    text:   'I want to use knockout',
+                    author: 'Unknown',
+                    date:   '10/10/2013',
+                    rate:   3
+                }
+            ]
+        };
+
+        $scope.voteUp = function (answer) {
+            answer.rate++;
+        };
+
+        $scope.voteDown = function (answer) {
+            answer.rate--;
+        };
+
+        $scope.questColorClass= "questcolor";
+
+        $scope.changeClass = function (e) {
+            $scope.questColorClass = e.type === "mouseover" ? "questselectedcolor" : "questcolor";
+        };
+    } );
+})();
