1abfa89ea125d37d81c3305023ec53af0a92ebf3
forest-shadow <mamasay@yandex.ru>
2017-07-13 12:19:06 +0300

    add example of ussing routes with params

--- ask-question-app--routing/app/app.router.js
+++ ask-question-app--routing/app/app.router.js
@@ -13,6 +13,14 @@
             templateUrl: './app/templates/answer.html',
             controller: 'AnswerController'
         })
+        .when("/edit/:id", {
+            templateUrl: "views/answer.html",
+            controller:'AnswerController'
+        })
+        .when("/edit/:id/:data*", {
+            templateUrl: "views/answer.html",
+            controller:'AnswerController'
+        })
         .otherwise({redirectTo: '/question'});
     });
 })();
--- ask-question-app--routing/app/controllers/answer.controller.js
+++ ask-question-app--routing/app/controllers/answer.controller.js
@@ -3,13 +3,30 @@
 (function(){
     let app = angular.module( 'askQuestion' );
 
-    app.controller( 'AnswerController', function AnswerController($scope, $http) {
-        $scope.response = {};
+    app.controller( 'AnswerController', function AnswerController($scope, $http, $location, $templateCache, $routeParams) {
+        $scope.answer = null;   
+         
+        $scope.$on('$routeChangeStart', function(event, next, current) {
+            if (typeof(current) !== 'undefined'){
+                $templateCache.remove(next.templateUrl);
+            }
+        });
+     
+        $scope.$on("$routeChangeSuccess", function () {
+            var id = $routeParams["id"]
+            if(id!=='undefined'){
+                
+                $http({method:'GET', url:'getAnswer.php', params: {'id':id}}).
+                then(function success(response) {
+                    $scope.answer = response.data;
+                });
+            }
+        });
 
-        $scope.save = function (answer, answerForm) {
-            if( answerForm.$valid ) {
-                $http.post( "postAnswer.php", answer ).then( function success( response ) {
-                    $scope.response = response.data;
+        $scope.save = function (answer, answerForm){
+            if(answerForm.$valid){
+                $http.post("postAnswer.php", answer).then(function success(response) {
+                    $location.path("question");
                 });
             }
         };
